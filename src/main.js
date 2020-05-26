47392543f5b13403301cfe20a81a9c41f45e8767
Daniel Subasic <subasic97@gmail.com>
2020-05-26 22:44:18 +0200

    La till Vue2-filters

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import Vuex from 'vuex'
 import 'materialize-css/dist/css/materialize.css'
 import 'materialize-css'
 import 'material-design-icons/iconfont/material-icons.css'
+import Vue2Filters from 'vue2-filters'
 
 Vue.use(VueRouter)
 Vue.use(Vuex)
+Vue.use(Vue2Filters)
 
 Vue.config.productionTip = false
 
