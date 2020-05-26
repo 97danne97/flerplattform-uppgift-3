0355014f05dc03c1452cf2b3884c71ced74663a0
Daniel Subasic <subasic97@gmail.com>
2020-05-26 22:31:18 +0200

    La till materialize som det ena externa biblioteket (jquery också, men bara för att materialize är beroende av det även om jquery kod inte används)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <div class="container">
   </div>
+    </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
+import 'materialize-css/dist/css/materialize.css'
+import 'materialize-css'
+import 'material-design-icons/iconfont/material-icons.css'
 
 Vue.use(VueRouter)
 Vue.use(Vuex)
