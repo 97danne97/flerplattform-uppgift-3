ef3b79f9b4e42ec4ee69154f59d54ac61709e4a4
Daniel Subasic <subasic97@gmail.com>
2020-06-01 01:11:53 +0200

    Stylade om drinksidan och hämtar nu information om drinkar från API:et när användaren kommer direkt till drinken via dess URL

--- src/views/Drink.vue
+++ src/views/Drink.vue
@@ -1,78 +1,112 @@
 <template>
-    <div class="section row" v-if="drink">
-        <div class="row">
-            <router-link class="btn btn-flat white waves-effect" :to="'/drinks'">
-                <i class="material-icons">arrow_back</i>
-            </router-link>
-        </div>
-        <div class="col s12 m6 xl5 center-align">
-            <img id="drink_img" class="responsive-img" :src="drink.strDrinkThumb" />
-        </div>
-        <div class="col s12 m6 xl7">
-            <h4 class="card-title">{{drink.strDrink}}</h4>
-            <div class="chip truncate pink white-text lighten-2">{{drink.strCategory}}</div>
-            <div class="chip truncate white-text purple lighten-1">{{drink.strGlass}}</div>
-            <div class="chip truncate white-text green lighten-1">{{drink.strAlcoholic}}</div>
-        </div>
-        <div id="ingredients" class="col s12 m6 xl7">
-            <h4>Ingredients</h4>
-            <div id="ingredients_list" class="col s12">
-                <div class="col" v-for="i in 15" :key="i" v-show="drink[strCombine('strIngredient', i)] != null">
-                    <div class="card z-depth-0 grey lighten-3">
-                        <div class="card-image">
-                            <img id="ingredient_img" :src="'https://www.thecocktaildb.com/images/ingredients/'+drink[strCombine('strIngredient', i)]+'-Small.png'" />
+    <div class="section drink_details_container" v-if="drink">
+        <div id="drink_details_card" class="card grey lighten-4 z-depth-0">
+            <div class="card-content">
+                <div class="row">
+                    <div class="col s12 center">
+                        <img id="drink_img" class="responsive-img" :src="drink.strDrinkThumb" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s12">
+                        <h4>{{drink.strDrink}}</h4>
+                        <div class="chip truncate grey lighten-2">{{drink.strCategory}}</div>
+                        <div class="chip truncate grey lighten-2">{{drink.strGlass}}</div>
+                        <div class="chip truncate grey lighten-2">{{drink.strAlcoholic}}</div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div id="ingredients" class="col s12">
+                        <h5>Ingredients</h5>
+                        <div id="ingredients_list" class="col s12">
+                            <div class="col" v-for="i in 15" :key="i" v-show='drink[strCombine("strIngredient", i)] != null && drink[strCombine("strIngredient", i)] != ""'>
+                                <div class="card z-depth-0 grey lighten-3">
+                                    <div class="card-image">
+                                        <img id="ingredient_img" :src="'https://www.thecocktaildb.com/images/ingredients/'+drink[strCombine('strIngredient', i)]+'-Small.png'" v-if="drink[strCombine('strIngredient', i)] != null" />
+                                    </div>
+                                </div>
+                                <div>
+                                    <p>{{drink[strCombine('strMeasure', i)]}}</p>
+                                    <p>{{drink[strCombine('strIngredient', i)]}}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                        <div class="">
-                            <h5 class="flow-text">{{drink[strCombine('strMeasure', i)]}}</h5>
-                            <span>{{drink[strCombine('strIngredient', i)]}}</span>
-                        </div>
+                </div>
+                <div class="row">
+                    <div id="mixing" class="col s12">
+                        <h5>Instructions</h5>
+                        <p>{{drink.strInstructions}}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div id="mixing" class="col">
-            <h4>Instructions</h4>
-            <blockquote>{{drink.strInstructions}}</blockquote>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "DrinkCard",
-        computed:{
-            drink () {
-                let x;
-                for (var i = 0; i < this.$store.state.drinks.length; i++) {
-                    console.log(this.$store.state.drinks[i])
-                    if (this.$store.state.drinks[i].idDrink == this.$route.params.id) {
-                        x = this.$store.state.drinks[i];
-                    }
-                }
-                return x;
-            },
-        },
-        methods: {
-            strCombine: function (k, i) {
-                k = k.toString()
-                i = i.toString()
-                let x = k+i
-                return x
-            }
+import axios from 'axios'
+
+export default {
+    name: "Drink",
+    data() {
+        return{
+            drink:false
         }
-    };
+    },
+    methods: {
+        strCombine: function (str, index) {
+            str = str.toString()
+            index = index.toString()
+            let x = str+index
+            return x
+        }
+    },
+    created(){
+        let drink;
+        let drink_id = this.$route.params.id;
+        if (this.$store.state.drinks.some(e => e['idDrink'] === drink_id)) {
+            for (let i = 0; i < this.$store.state.drinks.length; i++) {
+                console.log(this.$store.state.drinks[i])
+                if (this.$store.state.drinks[i].idDrink == drink_id) {
+                    drink = this.$store.state.drinks[i];
+                }
+            }
+            this.drink = drink;
+        }
+        else{
+            axios
+                .get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="+drink_id)
+                .then(res => {
+                    drink = res.data.drinks[0];
+                    console.log(drink)
+                    this.drink = drink;
+                });
+        }
+    }
+};
 </script>
 
 <style scoped>
+.drink_details_container{
+    max-width: 600px;
+    margin: auto;
+}
+#drink_img{
+    max-height: 200px;
+    margin: auto;
+}
+#drink_details_card{
+    outline: 1px solid rgb(219, 219, 219);
+}
+#ingredients_list{
+    display: flex;
+    overflow-x: auto;
+}
 
-    #ingredients_list{
-        display: flex;
-        overflow-x: auto;
-    }
-
-    #ingredient_img{
-        height: 100px;
-        padding: 10px;
-        width: 100px;
-    }
+#ingredient_img{
+    height: 90px;
+    padding: 10px;
+    width: 90px;
+}
 </style>
