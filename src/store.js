cf9ec8f48b5877a134cd5e64bf87657792101775
Daniel Subasic <subasic97@gmail.com>
2020-06-03 17:27:03 +0200

    Man kan nu sortera drinkarna efter typ eller om de innehåller alkohol, samt hämta fler drinkar när man når botten på listan

--- src/store.js
+++ src/store.js
@@ -9,10 +9,12 @@ export const store = new Vuex.Store({
         filters: [
             {
                 name: 'Drink type',
+                code: 'strCategory',
                 list: [{"strCategory":"Ordinary Drink"},{"strCategory":"Cocktail"},{"strCategory":"Milk / Float / Shake"},{"strCategory":"Other or Unknown"},{"strCategory":"Cocoa"},{"strCategory":"Shot"},{"strCategory":"Coffee / Tea"},{"strCategory":"Homemade Liqueur"},{"strCategory":"Punch / Party Drink"},{"strCategory":"Beer"},{"strCategory":"Soft / Drink / Soda"}]
             },
             {
                 name: 'Alcohol',
+                code: 'strAlcoholic',
                 list: [{"strAlcoholic":"Alcoholic"},{"strAlcoholic":"Non alcoholic"},{"strAlcoholic":"Optional alcohol"}]
             }
         ]
--- src/views/Drinks.vue
+++ src/views/Drinks.vue
@@ -10,25 +10,67 @@
                 <ul class="collapsible z-depth-0">
                     <li>
                         <div class="collapsible-header grey lighten-3 waves-effect">
-                            <i class="material-icons">settings</i>Filter
+                            <i class="material-icons">settings</i>Settings
                             <span class="new badge purple darken-2" :data-badge-caption="filter_by" v-show="filter_by.length > 0"></span>
                         </div>
                         <div class="collapsible-body grey lighten-5">
                             <form>
                                 <div class="row">
-                                    <label>
-                                        <input class="with-gap" id="none" type="radio" value="" v-model="filter_by"/>
-                                        <span>No filter</span>
-                                    </label>
-                                    <div class="col" v-for="(filter, index) in this.filters" :key="index">
-                                        <h5>{{filter.name}}</h5>
+                                    <div class="col s12">
+                                        <h3>Settings</h3>
                                         <div class="divider"></div>
-                                        <div class="row section">
-                                            <div class="col s12 m6" v-for="(item, index) in filter.list" :key="index">
-                                            <label class="black-text" :for="value" v-for="(value, key, index) in item" :key="index" v-on:click="filter_type=key">
-                                                <input class="with-gap" :id="value" type="radio" :value="value" v-model="filter_by"/>
-                                                <span>{{value}}</span>
-                                            </label>
+                                        <div class="col s12 m6">
+                                            <h4>Filters</h4>
+                                            <div class="col">
+                                                <label class="black-text">
+                                                    <input class="with-gap" id="none" type="radio" value="" v-model="filter_by"/>
+                                                    <span>No filter</span>
+                                                </label>
+                                            </div>
+                                            <div class="col" v-for="(filter, index) in this.filters" :key="index">
+                                                <h5>{{filter.name}}</h5>
+                                                <div class="divider"></div>
+                                                <div class="row section">
+                                                    <div class="col s12" v-for="(item, index) in filter.list" :key="index">
+                                                    <label class="black-text" :for="value" v-for="(value, key, index) in item" :key="index">
+                                                        <input class="with-gap" :id="value" type="radio" :value="value" v-model="filter_by"/>
+                                                        <span>{{value}}</span>
+                                                    </label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col s12 m6">
+                                            <h4>Sort</h4>
+                                            <div class="col">
+                                                <label class="black-text">
+                                                    <input class="with-gap" id="localId" type="radio" value="localId" v-model="sort_by.type"/>
+                                                    <span>Default</span>
+                                                </label>
+                                            </div>
+                                            <div class="col s12">
+                                                <h5>Mode</h5>
+                                                <div class="divider"></div>
+                                                <div class="row section">
+                                                    <div class="col s12" v-for="(filter, index) in this.filters" :key="index">
+                                                        <label class="black-text">
+                                                            <input class="with-gap" :id="filter.name" type="radio" :value="filter.code" v-model="sort_by.type"/>
+                                                            <span>{{filter.name}}</span>
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <h5>Direction</h5>
+                                                <div class="divider"></div>
+                                                <div class="row section">
+                                                    <div class="col s12" v-for="(sort_mode, index) in this.sort_modes" :key="index">
+                                                        <label class="black-text" :for="sort_mode.name+'_'+sort_mode.value">
+                                                            <input class="with-gap" :id="sort_mode.name+'_'+sort_mode.value" type="radio" :value="sort_mode.value" v-model="sort_by.mode"/>
+                                                            <span>{{sort_mode.name}}</span>
+                                                        </label>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -40,8 +82,13 @@
             </div>
         </div>
         <transition-group name="list" tag="div" class="row" id="drinks_list">
-            <DrinkCard class="list-item" v-for="drink in filterBy(filterItems)" :key="drink.idDrink" :drink="drink"></DrinkCard>
+            <DrinkCard class="list-item" v-for="drink in orderBy(filterBy(filterItems), this.sort_by.type.toString(), this.sort_by.mode)" :key="drink.idDrink" :drink="drink"></DrinkCard>
         </transition-group>
+        <div class="row">
+            <div class="col s12 center">
+                <button v-if="this.filter_by == ''" v-on:click="randomDrinks(8)" class="btn purple darken-2 waves-effect waves-light"><i class="material-icons left">add</i>load more</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -61,14 +108,19 @@ export default {
         return {
             instances: [],
             filter_by: '',
-            filter_type: undefined,
+            sort_by: {
+                type:'localId',
+                mode: 0
+            },
+            sort_modes: [
+                {name:'Ascending (Default)', value: 0},
+                {name:'Descending',value: -1}
+            ],
             search_query: '',
+            loading: true,
         };
     },
     computed: {
-        favorite_drinks() {
-            return this.$store.state.drinks;
-        },
         filters() {
             return this.$store.state.filters;
         },
@@ -118,26 +170,38 @@ export default {
                 .get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + query)
                 .then(res => {
                     this.onResponse(res.data.drinks, 'set');
+                    this.loading = false;
                 });
         },
         onResponse: function (drinks, mode){
             let self = this;
-            console.log(drinks)
+            drinks.forEach(function(drink){
+                drink.localId = drinks.length;
+            });
+            console.log(drinks);
             if (mode == 'set'){
                 self.$store.state.drinks = drinks;
             }else if(mode == 'append'){
                 drinks.forEach(function(drink){
-                    self.$store.state.drinks.push(drink);
+                    if (self.$store.state.drinks.some(e => e['idDrink'] === drink.idDrink)) {
+                        console.log('Drink already in list')
+                    }
+                    else{
+                        self.$store.state.drinks.push(drink);
+                        drink.localId = self.$store.state.drinks.length;
+                    }
                 });
             }
         },
-        randomDrinks: function (amount = 10){
+        randomDrinks: function (amount = 8){
+            this.loading = true;
             //GET-request till API för att hämta slumpade drinkar
             for (let i = amount; i--; i > 0 ) {
                 axios
                     .get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
                     .then(res => {
                         this.onResponse(res.data.drinks, 'append'); // Kallar på onResponse vid svar från API
+                        this.loading = false;
                     });
             }
         }
